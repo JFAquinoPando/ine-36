163796754fde32ae36d411e33b710d2928e787a5
JFAquinoPando <pandolcik@gmail.com>
2025-02-14 20:03:11 -0300

    Ahora veamos como endpoint

--- index_bkp.js
+++ index_bkp.js
@@ -1,6 +1,49 @@
 import { PrismaClient } from "@prisma/client";
+import express from "express";
+import cors from "cors";
 const prisma = new PrismaClient();
-(
+const aplicacion = express()
+aplicacion.use(cors())
+aplicacion.use(express.json())
+
+const PUERTO = 3000
+
+aplicacion.get("/", (peticion, respuesta) => {
+    respuesta.send("Hola")
+})
+
+aplicacion.get("/usuarios", async (peticion, respuesta) => {
+    try {
+        const usuarioObjeto = {
+            data: {
+                email: 'prueba1@idt.com.py',
+                edad: 47
+            },
+        }
+
+        
+
+       /*  const actualizarUser2 = await prisma.usuario.update({
+            where : { email: "aquino@idt.com.py" },
+            data: { full: "completo", edad: 60 }
+        })
+        console.log("usuario actualizado2", actualizarUser2); */
+
+
+        const crearUser = await prisma.usuario.create(usuarioObjeto)
+        respuesta.send(crearUser)
+        console.log("Usuario creado", crearUser);
+
+
+    } catch (error) {
+        console.error("Hay un error", error);
+    } finally {
+        await prisma.$disconnect()
+    }
+})
+
+
+/* (
     async () => {
         try {
             const usuarioObjeto = {
@@ -12,11 +55,11 @@ const prisma = new PrismaClient();
 
             
 
-           /*  const actualizarUser2 = await prisma.usuario.update({
-                where : { email: "aquino@idt.com.py" },
-                data: { full: "completo", edad: 60 }
-            })
-            console.log("usuario actualizado2", actualizarUser2); */
+           //  const actualizarUser2 = await prisma.usuario.update({
+           //     where : { email: "aquino@idt.com.py" },
+           //     data: { full: "completo", edad: 60 }
+           // })
+           // console.log("usuario actualizado2", actualizarUser2);
 
 
             const crearUser = await prisma.usuario.create(usuarioObjeto)
@@ -30,4 +73,4 @@ const prisma = new PrismaClient();
         }
     }
 
-)();
+)(); */
